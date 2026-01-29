8f1f2a1823394326913920d6258b06a66d815fb8
manthan3319 <vaghasiyamanthan8@gmail.com>
2026-01-29 12:55:02 +0530

    use render port

--- server.js
+++ server.js
@@ -65,4 +65,6 @@ app.use("/api/faq", faqRoutes);
 app.use("/api/faq2", faq2Routes);
 app.use("/api/contact-form", contactFormRoutes);
 app.use("/api/dashboard", dashboardRoutes);
-app.listen(5000, () => console.log("server running on 5000"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
